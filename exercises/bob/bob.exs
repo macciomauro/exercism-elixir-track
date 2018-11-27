1d6fb5606fcc93009826bc5ef3fb02d2e9ddd5d5
Mauro Macciò <mauro.maccio@prima.it>
2018-11-27 15:50:05 +0100

    Refactor "Bob" exercise

--- exercises/bob/bob.exs
+++ exercises/bob/bob.exs
@@ -1,13 +1,28 @@
 defmodule Bob do
   def hey(input) do
-    has_only_numbers = String.match?(input, ~r/^[0-9, ?]*$/)
 
     cond do
-      String.trim(input) == "" -> "Fine. Be that way!"
-      String.upcase(input) == input && String.ends_with?(input, "?") && !has_only_numbers -> "Calm down, I know what I'm doing!"
-      String.ends_with?(input, "?") -> "Sure."
-      String.upcase(input) == input && !has_only_numbers -> "Whoa, chill out!"
+      isEmpty(input) -> "Fine. Be that way!"
+      isYelledSentence(input) && isQuestion(input) -> "Calm down, I know what I'm doing!"
+      isYelledSentence(input) -> "Whoa, chill out!"
+      isQuestion(input) -> "Sure."
       true -> "Whatever."
     end
+  end
+
+  defp isEmpty(input) do
+    String.trim(input) == ""
+  end
+
+  defp hasOnlyNumbers(input) do
+    String.match?(input, ~r/^[0-9, ?]*$/)
+  end
+
+  defp isQuestion(input) do
+    String.ends_with?(input, "?")
+  end
+
+  defp isYelledSentence(input) do
+    String.upcase(input) == input && !hasOnlyNumbers(input)
   end
 end

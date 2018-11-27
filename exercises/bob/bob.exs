930d0a6be1251cf4902e5b988b8765c9f9a649a2
Mauro Macciò <mauro.maccio@prima.it>
2018-11-27 17:07:31 +0100

    Refactor "bob" functions names

--- exercises/bob/bob.exs
+++ exercises/bob/bob.exs
@@ -2,27 +2,27 @@ defmodule Bob do
   def hey(input) do
 
     cond do
-      isEmpty(input) -> "Fine. Be that way!"
-      isYelledSentence(input) && isQuestion(input) -> "Calm down, I know what I'm doing!"
-      isYelledSentence(input) -> "Whoa, chill out!"
-      isQuestion(input) -> "Sure."
+      empty?(input) -> "Fine. Be that way!"
+      yelled_sentence?(input) && question?(input) -> "Calm down, I know what I'm doing!"
+      yelled_sentence?(input) -> "Whoa, chill out!"
+      question?(input) -> "Sure."
       true -> "Whatever."
     end
   end
 
-  defp isEmpty(input) do
+  defp empty?(input) do
     String.trim(input) == ""
   end
 
-  defp hasOnlyNumbers(input) do
+  defp only_numbers?(input) do
     String.match?(input, ~r/^[0-9, ?]*$/)
   end
 
-  defp isQuestion(input) do
+  defp question?(input) do
     String.ends_with?(input, "?")
   end
 
-  defp isYelledSentence(input) do
-    String.upcase(input) == input && !hasOnlyNumbers(input)
+  defp yelled_sentence?(input) do
+    String.upcase(input) == input && !only_numbers?(input)
   end
 end

32301bd9a94462c9c5072c7b47d021746449d614
Mauro Macciò <mauro.maccio@prima.it>
2018-11-29 10:10:10 +0100

    Better "Bob" exercise

--- teams/primait/elixir/bob/bob.exs
+++ teams/primait/elixir/bob/bob.exs
@@ -2,27 +2,11 @@ defmodule Bob do
   def hey(input) do
 
     cond do
-      empty?(input) -> "Fine. Be that way!"
-      yelled_sentence?(input) && question?(input) -> "Calm down, I know what I'm doing!"
-      yelled_sentence?(input) -> "Whoa, chill out!"
-      question?(input) -> "Sure."
+      "" == String.trim(input) -> "Fine. Be that way!"
+      input == String.upcase(input) && !String.match?(input, ~r/^[0-9, ?]*$/) && String.ends_with?(input, "?") -> "Calm down, I know what I'm doing!"
+      input == String.upcase(input) && !String.match?(input, ~r/^[0-9, ?]*$/) -> "Whoa, chill out!"
+      String.ends_with?(input, "?") -> "Sure."
       true -> "Whatever."
     end
-  end
-
-  defp empty?(input) do
-    String.trim(input) == ""
-  end
-
-  defp only_numbers?(input) do
-    String.match?(input, ~r/^[0-9, ?]*$/)
-  end
-
-  defp question?(input) do
-    String.ends_with?(input, "?")
-  end
-
-  defp yelled_sentence?(input) do
-    String.upcase(input) == input && !only_numbers?(input)
   end
 end
